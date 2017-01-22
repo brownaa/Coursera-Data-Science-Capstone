7eeabcdd016bea9b6a737e64c891f6d2216992bf
Aaron Brown <brownaa@mac.com>
2017-01-22 12:40:41 -0600

    updating EDA1

--- src/01 - Fetch Raw Data.R
+++ src/01 - Fetch Raw Data.R
@@ -1,14 +1,18 @@
-## Fetch raw data
-if (!require(pacman)) install.packages('pacman')
-pacman::p_load('RCurl')
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
+## This script only needs to be run once in order to download the dataset   ##
+## and clean up the unzipped directories                                    ##
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
 
 dir.create("data")
-Sys.chmod(file.path(getwd(),"data"), mode = "775", use_umask = FALSE) #modify directory permissions
+Sys.chmod(file.path(getwd(),"data"), 
+          mode = "775", #modify permission of the folder to allow writing
+          use_umask = FALSE)
 download.file(url = "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip",
               destfile = "./data/Coursera-SwiftKey.zip")
 
 unzip(zipfile = file.path(getwd(),"data/Coursera-SwiftKey.zip"),
       exdir = file.path(getwd(), "data"))
+
 # move files out of the "final" directory into the "data" directory
 file.rename(from = file.path(getwd(), "data/final/de_DE"),
             to   = file.path(getwd(), "data/de_DE"))
--- src/EDA1.R
+++ src/EDA1.R
@@ -1,12 +1,16 @@
+options( java.parameters = "-Xmx4g" )
+
 if (!require(pacman)) install.packages('pacman')
 pacman::p_load('ProjectTemplate')
 load.project()
 
-##load US blogs
-con = file(description = "./data/en_US/en_us.blogs.txt")
-us.blogs = readLines(con = con)
-close(con)
+us_blog = Corpus(DirSource("./data/en_US/", pattern = "blog"))
 
-options( java.parameters = "-Xmx4g" )
-words = WordTokenizer(us.blogs)
-
+us_blog <- tm_map(us_blog, content_transformer(tolower)) 
+us_blog <- tm_map(us_blog, removeWords, stopwords("english")) 
+us_blog <- tm_map(us_blog, removePunctuation) 
+us_blog <- tm_map(us_blog, removeNumbers) 
+us_blog <- tm_map(us_blog, stripWhitespace) 
+us_blog_stemmed <- tm_map(us_blog, stemDocument) #stemmed
+cntl = cntr <- list(wordLengths = c(3, Inf))
+dtm <- DocumentTermMatrix(us_blog, control = cntl)
