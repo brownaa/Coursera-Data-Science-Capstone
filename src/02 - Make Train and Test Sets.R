3db141d2dbbae86e7cbf5e2b460a0033b24bc4c5
Aaron Brown <brownaa@mac.com>
2017-07-02 16:24:00 -0500

    cleaned up code after last error fix

--- src/02 - Make Train and Test Sets.R
+++ src/02 - Make Train and Test Sets.R
@@ -11,13 +11,7 @@ load.project()
 split_tsets = function(corp, trainfrac = 0.8) {
     corp_length = length(corp)
     
-    # to_newline <- content_transformer(function (x) unlist(strsplit(x, "\n")))
-    # contents = unlist(strsplit(corp[[i]]$content, "\n"))
-    
-    # corp = tm_map(corp, to_newline)
-    
     for(i in 1:corp_length){
-        # corp[[i]]$content = contents
         len = length(corp[[i]]$content)
         tset = as.character(rbinom(len, 1, trainfrac))
         tset[tset == "1"] = "train"
